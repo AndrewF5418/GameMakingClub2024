d75717bcb50cffb92ae4dcf9e7b248c7c245b27d
AndrewF5418 <fang13612978925@outlook.com>
2024-02-29 17:34:41 +0800

    Animation function and new art asset
    Update the animation function. When player moves, the animation is playing animator files. The code use the detection of GetAxisRaw to make boolean variable that control the play of animation. This feature has a bug that the animation cannot be played correctly when player doing x and y axis motion at the same time. The art asset are draw by Jingyu.

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //trait
     public float moveSpeed;
-
+    //animator
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-
+        //anim = GetComponent(Animator);
+        anim.SetBool("isMovingUpDown",false);
+        anim.SetBool("isMovingLeft",false);
+        anim.SetBool("isMovingRight",false); 
     }
 
     // Update is called once per frame
@@ -20,8 +25,33 @@ public class PlayerController : MonoBehaviour
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
         transform.position += moveInput * moveSpeed*Time.deltaTime;
-    
-    }
+        // set bool for moving animation by if condition 
+        if(moveInput != Vector3.zero){
+            if(moveInput.x>0 ||
+            (moveInput.x>0 && Input.GetAxisRaw("Vertical")==1)||
+            (moveInput.x>0 && Input.GetAxisRaw("Vertical")==-1)
+            )
+        {
+            anim.SetBool("isMovingRight",true);
+        }
+            if(moveInput.x<0||
+            (moveInput.x<0 && Input.GetAxisRaw("Vertical")==1)||
+            (moveInput.x<0 && Input.GetAxisRaw("Vertical")==-1)
+            )
+        {
+            anim.SetBool("isMovingLeft",true);
+        }
+            if(Input.GetAxisRaw("Vertical")==1 || Input.GetAxisRaw("Vertical")==-1)
+        {
+            anim.SetBool("isMovingUpDown",true);
+        }
+        }
 
-   
+        else{
+            anim.SetBool("isMovingUpDown",false);
+            anim.SetBool("isMovingLeft",false);
+            anim.SetBool("isMovingRight",false); 
+        }    
+
+}
 }
